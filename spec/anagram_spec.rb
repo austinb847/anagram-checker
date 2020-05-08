b23d4f5572a4383cd5ce437714d2af72a5262b16
austinb847 <austinb847@icloud.com>
2020-05-08 11:09:17 -0700

    meaningful commit for check_for_anagram method.

--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -7,3 +7,10 @@ describe("Word#check_for_vowels") do
     expect(word.check_for_vowels('tlchk')).to(eq(false))
   end
 end
+
+describe("Word#check_for_anagram") do
+  it("should return 'You need to input actual words' if either word has no vowels") do
+    word = Word.new()
+    expect(word.check_for_anagram('hi', 'tlchk')).to(eq('you need to input actual words'))
+  end
+end
