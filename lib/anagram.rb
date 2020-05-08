4ebb627cfebdaff643537c49a81624926366585b
austinb847 <austinb847@icloud.com>
2020-05-08 12:17:21 -0700

    check if both words have the same letters.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -12,9 +12,14 @@ class Word
   def check_for_anagram(word1, word2)
     word1 = word1.downcase
     word2 = word2.downcase
-    
+
     if !check_for_vowels(word1) || !check_for_vowels(word2)
       return "You need to input actual words"
+    end
+
+    word1_letters = word1.chars
+    if word1_letters.all?{|l| word2.chars.include?(l)}
+      return "These words are anagrams"
     end
   end
 end
