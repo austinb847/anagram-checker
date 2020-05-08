d450bb8c06838e8b6136e1656eb0f485e3170923
austinb847 <austinb847@icloud.com>
2020-05-08 10:59:21 -0700

    return false if word has no vowels

---
+++ lib/anagram.rb
@@ -0,0 +1,15 @@
+class Word
+
+
+  def check_for_vowels(word)
+    if word.count("aeiouy") > 0
+      true
+    else
+      false
+    end
+  end
+
+  # def check_for_anagram(word1, word2)
+
+  # end
+end
