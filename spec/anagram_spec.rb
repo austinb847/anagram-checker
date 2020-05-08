56009bf80eeb18785c85297ebe2eda2570150d5d
austinb847 <austinb847@icloud.com>
2020-05-08 11:35:15 -0700

    meaningful fail test for check_for_anagram

--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -11,6 +11,12 @@ end
 describe("Word#check_for_anagram") do
   it("should return 'You need to input actual words' if either word has no vowels") do
     word = Word.new()
-    expect(word.check_for_anagram('hi', 'tlchk')).to(eq('you need to input actual words'))
+    expect(word.check_for_anagram('hi', 'tlchk')).to(eq('You need to input actual words'))
   end
+
+  it("should return 'These words are anagrams' if both words contain the same letters in a differnt order") do
+    word = Word.new()
+    expect(word.check_for_anagram('ruby', 'bury')).to(eq('These words are anagrams'))
+  end
+
 end
